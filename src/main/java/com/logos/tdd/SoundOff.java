0bfcb77a9918fa3ef7263a64ec43b4bf3fce11c5
shiwei.fu <shiwei.fu@thoughtworks.com>
2020-01-09 21:11:24 +0800

    Initial commit

---
+++ src/main/java/com/logos/tdd/SoundOff.java
@@ -0,0 +1,14 @@
+package com.logos.tdd;
+
+import java.util.stream.IntStream;
+
+public class SoundOff {
+
+  public static void main(String[] args) {
+    IntStream.range(1, 1024).forEach(number -> System.out.println(fizzBuzz(number)));
+  }
+
+  private static String fizzBuzz(int number) {
+    return null;
+  }
+}
---
+++ src/test/java/com/logos/tdd/SoundOffTest.java
@@ -0,0 +1,12 @@
+package com.logos.tdd;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SoundOffTest {
+
+  @Test
+  void name() {
+  }
+}
