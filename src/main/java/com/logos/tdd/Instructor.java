75b418704f5c2296955046e77714947c03c3ab94
shiwei.fu <shiwei.fu@thoughtworks.com>
2020-01-10 08:25:14 +0800

    :recycle: Refactoring code.

--- src/main/java/com/logos/tdd/Instructor.java
+++ src/main/java/com/logos/tdd/Instructor.java
@@ -1,5 +1,11 @@
 package com.logos.tdd;
 
+import static com.logos.tdd.DividendENUM.Buzz;
+import static com.logos.tdd.DividendENUM.Fizz;
+import static com.logos.tdd.DividendENUM.Whizz;
+import static java.util.stream.Collectors.joining;
+
+import java.util.Arrays;
 import java.util.stream.IntStream;
 import org.apache.commons.lang3.StringUtils;
 
@@ -11,45 +17,33 @@ public class Instructor {
 
   protected static String soundOff(Integer number) {
 
-    if (IsNumberHasSpecialCharacters(number, "7")) {
-      return ruleForNumberHas7(number);
+    if (isNumberHasSpecialCharacters(number, "7")) {
+      return ruleStrategy(number, Fizz, Whizz);
     }
-    if (IsNumberHasSpecialCharacters(number, "5")) {
-      return ruleForNumberHas5(number);
+    if (isNumberHasSpecialCharacters(number, "5")) {
+      return ruleStrategy(number, Buzz, Whizz);
     }
-    if (IsNumberHasSpecialCharacters(number, "3")) {
-      return DividendENUM.Fizz.name();
+    if (isNumberHasSpecialCharacters(number, "3")) {
+      return Fizz.name();
     }
-    return NormalMultipleRule(number);
+    return normalMultipleRule(number);
   }
 
-  private static String ruleForNumberHas7(Integer number) {
-    String soundOff = "";
-    soundOff += ruleForMultiple(number, DividendENUM.Fizz);
-    soundOff += ruleForMultiple(number, DividendENUM.Whizz);
-    return soundOff;
-  }
-
-  private static String ruleForNumberHas5(Integer number) {
-    String soundOff = "";
-    soundOff += ruleForMultiple(number, DividendENUM.Buzz);
-    soundOff += ruleForMultiple(number, DividendENUM.Whizz);
-    return soundOff;
-  }
-
-  private static String NormalMultipleRule(Integer number) {
-    String soundOff = "";
-    soundOff += ruleForMultiple(number, DividendENUM.Fizz);
-    soundOff += ruleForMultiple(number, DividendENUM.Buzz);
-    soundOff += ruleForMultiple(number, DividendENUM.Whizz);
+  private static String normalMultipleRule(Integer number) {
+    String soundOff = ruleStrategy(number, Fizz, Buzz, Whizz);
     return StringUtils.isBlank(soundOff) ? number.toString() : soundOff;
+  }
+
+  private static String ruleStrategy(Integer number, DividendENUM... dividends) {
+    return Arrays.stream(dividends).map(dividend -> ruleForMultiple(number, dividend))
+        .collect(joining());
   }
 
   private static String ruleForMultiple(Integer number, DividendENUM dividend) {
     return isNumberMultipleOfDividend(number, dividend.getDividend()) ? dividend.name() : "";
   }
 
-  private static boolean IsNumberHasSpecialCharacters(Integer number, String specialCharacters) {
+  private static boolean isNumberHasSpecialCharacters(Integer number, String specialCharacters) {
     return number.toString().contains(specialCharacters);
   }
 
