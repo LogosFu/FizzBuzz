91ab0f3327426746b0942b5ba84bed628fedd2a8
shiwei.fu <shiwei.fu@thoughtworks.com>
2020-01-11 23:24:03 +0800

    :tada: Initial commit.

--- src/test/java/com/logos/tdd/SoundOffTest.java
+++ src/test/java/com/logos/tdd/SoundOffTest.java
@@ -1,12 +1,11 @@
 package com.logos.tdd;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
 class SoundOffTest {
 
   @Test
-  void name() {
+  void shouldReturnNumberWhenSoundOffGivenStudentNumber() {
+
   }
 }
