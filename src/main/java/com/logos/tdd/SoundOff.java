29d9fcf019eeaf979a2f31cdbe8660418e315294
shiwei.fu <shiwei.fu@thoughtworks.com>
2020-01-12 20:34:10 +0800

    Use

--- src/main/java/com/logos/tdd/SoundOff.java
+++ src/main/java/com/logos/tdd/SoundOff.java
@@ -5,10 +5,7 @@ import java.util.stream.IntStream;
 public class SoundOff {
 
   public static void main(String[] args) {
-    IntStream.range(1, 1024).forEach(number -> System.out.println(fizzBuzz(number)));
-  }
-
-  private static String fizzBuzz(int number) {
-    return null;
+    IntStream.range(1, 1024).mapToObj(Student::new).map(Student::soundOff).forEach(
+        System.out::println);
   }
 }
